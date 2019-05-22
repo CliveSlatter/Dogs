be8105e9c983e17b6b3d240fe805299a417a8ed3
CliveSlatter <cslatter@farnborough.ac.uk>
2019-05-22 12:31:43 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Dog> dogs = new ArrayList<>();
+
         Dog charlie = new Dog("Charlie","Retriever",7,"male","Pale Yellow");
         Dog evie = new Dog("Evie","Labrador", 5, "female","Black");
         System.out.println(charlie.noise());
@@ -9,5 +13,11 @@ public class Main {
         System.out.println(evie);
         System.out.println(charlie.sit());
         System.out.println(evie.sit());
+
+        dogs.add(charlie);
+        dogs.add(evie);
+
+        for(Dog d : dogs) System.out.println(d);
+
     }
 }
